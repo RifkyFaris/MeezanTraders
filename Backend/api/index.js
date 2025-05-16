b6ec8b02407a58648caa6d7afcccde7f872cf2fb
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-05-16 21:26:44 +0530

    Delete Backend/api directory

--- Backend/api/index.js
+++
@@ -1,8 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../app');
-const connectDatabase = require('../config/database');
-
-// Connect DB (once per cold start)
-connectDatabase();
-
-module.exports = serverless(app);
