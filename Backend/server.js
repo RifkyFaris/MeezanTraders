dcdc98c415e35e0b21477cc017643965fc14ab35
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-05-16 19:23:26 +0530

    Delete Backend/server.js

--- Backend/server.js
+++
@@ -1,8 +0,0 @@
-const app=require("./app");
-const connectDatabase = require("./config/database");
-
-connectDatabase();
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server listening to port ${process.env.PORT}`)
-})
