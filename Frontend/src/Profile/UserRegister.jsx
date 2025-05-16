806c943f242b9ad96f8242f98b3a4d8e1847edfe
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-05-16 18:45:44 +0530

    Update UserRegister.jsx

--- Frontend/src/Profile/UserRegister.jsx
+++ Frontend/src/Profile/UserRegister.jsx
@@ -5,7 +5,6 @@ import { Link,useNavigate } from 'react-router-dom'
 import {useDispatch,useSelector} from 'react-redux'
 import {register,clearAuthError} from './actions/userActions.jsx'
 import {toast} from 'react-toastify'
-import MetaData from '../components/MetaData'
 
 const UserRegister = () => {
   const [userData,setUserData]=useState({
