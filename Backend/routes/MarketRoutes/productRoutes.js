ca155026b10046d2c05a2d545fcf5b9ef7e9e306
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-05-18 17:19:09 +0530

    Add files via upload

--- Backend/routes/MarketRoutes/productRoutes.js
+++ Backend/routes/MarketRoutes/productRoutes.js
@@ -15,19 +15,12 @@ const {isAuthenticatedUser,authorizeRoles} =require("../../middlewares/ProfileMi
 const multer = require('multer');
 const path = require('path');
 
-const upload = multer({storage: multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, path.join( __dirname,'..' ,'..', 'uploads/MarketUploads/Product' ) )
-    },
-    filename: function(req, file, cb ) {
-        cb(null, file.originalname)
-    }
-}) })
+
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/supplier/product/new').post(isAuthenticatedUser,authorizeRoles('supplier'),upload.array('images'),newProduct)
-router.route('/supplier/update/:id').put(isAuthenticatedUser,authorizeRoles('supplier'),upload.array('images'),updateProducts)
+router.route('/supplier/product/new').post(isAuthenticatedUser,authorizeRoles('supplier'),newProduct)
+router.route('/supplier/update/:id').put(isAuthenticatedUser,authorizeRoles('supplier'),updateProducts)
 router.route('/supplier/product/:id').delete(isAuthenticatedUser,authorizeRoles('supplier','admin'),deleteProduct)
 router.route('/supplier/products').get(isAuthenticatedUser,authorizeRoles('supplier'),getSupplierProducts)
 
