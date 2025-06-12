e627e16d48d490d6757c982cc347d3af1149ed9e
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-06-12 22:26:11 +0530

    Update Product.jsx

--- Frontend/src/Market/Product.jsx
+++ Frontend/src/Market/Product.jsx
@@ -3,6 +3,7 @@ import '../Profile/Login.css'
 export default function Product({product}){
     return(
         <div className="product">
+              <img className="product-image"  src={product.images[0].image} alt="product"/>
               
               <div className="product-details">
                 <p className="product-name">{product.name}</p>
