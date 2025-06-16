408b96df7f37b3d67aeb90c5f02bb1a4fcb8bf9e
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-06-16 11:31:36 +0530

    Add files via upload

--- Backend/app.js
+++ Backend/app.js
@@ -2,16 +2,18 @@ const express =require('express');
 const cors = require('cors'); 
 const app=express();
 const dotenv=require('dotenv');
-
 const path=require('path')
+
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 const cookieParser=require('cookie-parser')
 const profile=require('./routes/ProfileRoutes/profile.js')
 const product=require('./routes/MarketRoutes/productRoutes.js')
 const order=require('./routes/MarketRoutes/order.js')
-const payment=require('./routes/MarketRoutes/paymentRoutes.js')
-const connectDatabase = require('./config/database');
-connectDatabase();
+
+
+
+
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -20,12 +22,9 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-
 app.use('/uploads',express.static(path.join(__dirname,'uploads')))
 
-app.use('/api/profile',profile);
-app.use('/api/product',product);
-app.use('/api/order',order);
-app.use('/api/payment',payment);
-
+app.use('/',profile);
+app.use('/',product);
+app.use('/',order);
 module.exports=app;
