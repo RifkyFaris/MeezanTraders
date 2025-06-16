209ef88eb3c7d49a05ca5b665954ae606860f171
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-06-16 12:35:25 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/": "https://meezantraders.vercel.app"
+      "/api": "https://meezantraders.vercel.app"
     }
   },
   
