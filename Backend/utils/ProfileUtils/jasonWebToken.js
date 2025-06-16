8d411443fb4e772360008820957d452cd40bbce7
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-06-16 11:30:59 +0530

    Add files via upload

--- Backend/utils/ProfileUtils/jasonWebToken.js
+++ Backend/utils/ProfileUtils/jasonWebToken.js
@@ -9,9 +9,6 @@ const sendToken = (user, statusCode, res) => {
                 Date.now() + process.env.COOKIE_EXPIRES_TIME  * 24 * 60 * 60 * 1000 
             ),
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',  // true only in production
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-  path: '/',
     }
 
     res.status(statusCode)
