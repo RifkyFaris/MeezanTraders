343142c2d8acc0f66281b3b46753a2ea9d986ca9
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-06-16 11:29:06 +0530

    Add files via upload

--- Backend/routes/MarketRoutes/order.js
+++ Backend/routes/MarketRoutes/order.js
@@ -2,24 +2,24 @@ const express=require('express');
 const router=express.Router();
 const {newOrder,
     getSingleOrder,
-    getSupplierOrders,
     myOrders,
     orders,
     updateOrder,
-    deleteOrder
+    deleteOrder,
+    getProcessingOrders
 }=require("../../controllers/MarketController/orderController.js")
-const {isAuthenticatedUser,authorizeRoles}=require('../../middlewares/ProfileMiddlewares/authenticate.js')
+const {isAuthenticatedUser,authorizeRoles}=require('../../middlewares/authenticate.js')
 
-router.route('/order/new').post(isAuthenticatedUser,newOrder);
-router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder);
-router.route('/myorder').get(isAuthenticatedUser,myOrders);
+router.route('/api/order/new').post(isAuthenticatedUser,newOrder);
+router.route('/api/order/:id').get(isAuthenticatedUser,getSingleOrder);
+router.route('/api/myorder').get(isAuthenticatedUser,myOrders);
+router.route('/api/processing').get(isAuthenticatedUser,authorizeRoles('admin'),getProcessingOrders);
 
-router.route('/admin/orders').get(isAuthenticatedUser,authorizeRoles('admin'),orders);
-router.route('/admin/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder);
+router.route('/api/admin/orders').get(isAuthenticatedUser,authorizeRoles('admin'),orders);
+router.route('/api/admin/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder);
 
-router.route('/admin/delete/order/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteOrder);
+router.route('/api/admin/delete/order/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteOrder);
 
-router.route('/supplier/order').get(isAuthenticatedUser,authorizeRoles('supplier'),getSupplierOrders);
 
 
 
