c6956ddc952565b20ecd73a3a47d9dc44a972e98
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-08-05 08:37:15 +0530

    Add files via upload

---
+++ Frontend/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="loader-container">
+      <img src="/meezanlogo.png" alt="Meezan Traders" className="loader-logo" />
+    </div>
+    )
+}
