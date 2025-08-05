8e20925ac1b8abe9253ce02184eee3e47713f593
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2025-08-05 10:10:04 +0530

    Update Loader.jsx

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,9 +1,10 @@
 export default function Loader() {
     return (
-        <div className='container'>
+        <div className='container' style={{ marginTop: '-10px' }}>
         <div className="loader-container">
       <img src="/meezanlogo.png" alt="Meezan Traders" className="loader-logo" />
     </div>
     </div>
     )
+
 }
